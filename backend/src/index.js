2f8cf428fa58f48883f44543f02101094a931983
jules <jsoaresandrie@gmail.com>
2020-03-25 03:03:36 -0300

    Back-end da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express =  require('express');
+const cors = require('cors')
 const routes = require('./routes');
 
 const app = express();
 
+//quando estiver em produção
+// app.use(cors({
+//     origin: meuapp.com.br
+// }));
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
